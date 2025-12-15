cb7d9e41255a0d3133ba1af7b91b91549695a711
C-Reaper <gnu.c.reaper@gmail.com>
2025-12-15 18:07:29 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,3 +1,4 @@
+/*
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
 #include "/home/codeleaded/System/Static/Library/ObjectNotation.h"
 
@@ -125,33 +126,223 @@ void EDisplay_Free(EDisplay* d){
     TextBox_Free(&d->Field);
 }
 
+*/
 
-Vector displays;
+#include "/home/codeleaded/System/Static/Container/CVector.h"
+
+#include "/home/codeleaded/System/Static/Library/ComponentPack.h"
+#include "/home/codeleaded/System/Static/Library/ComponentML.h"
+#include "/home/codeleaded/System/Static/Library/ComponentDefines.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/FileChooser.h"
+
+Scene scene;
+ComponentPack cg;
+ComponentML cml;
+CStr fileinBuffer;
+CVector filesOpen;
+
+void Button_File_EventHandler(void* parent,Button* b,ButtonEvent* e){
+    if(e->ButtonId == ALX_MOUSE_L && e->eid == EVENT_PRESSED){
+        CStr text = String_CStr(&b->text);
+        printf("%s -> L\n",text);
+        CStr_Free(&text);
+    }
+}
+void Button_Open_EventHandler(Button* b,Component* c,ButtonEvent* e){
+    if(e->ButtonId == ALX_MOUSE_L && e->eid == EVENT_PRESSED){
+        FileChooser fc = FileChooser_New("","/home/codeleaded/Hecke/C/Gui_IDE_New/code",(NameTypePair[]){
+            NameTypePair_New("All Files","*.*"),
+            NameTypePair_Null()
+        });
+        CStr_Set(&fileinBuffer,fc);
+        CVector_Push(&filesOpen,(CStr[]){ CStr_Cpy(fileinBuffer) });
+
+        printf("Choosen: '%s'\n",fileinBuffer);
+
+        CStr name = Files_NameFull(fileinBuffer);
+        const int count = scene.size - 3;
+        const int width = 150;
+        Scene_Add(&scene,(Button[]){ Button_NewStd(
+            NULL,
+            name,
+            (void(*)(void*,Label*,LabelEvent*))Button_File_EventHandler,
+            (Vec2){ 16,16 },
+            (Rect){ 100 + count * width,0.0f,width,20.0f },
+            0xFF000044,
+            0xFFAAAAAA
+            )},sizeof(Button)
+        );
+
+        CStr_Free(&name);
+        FileChooser_Free(&fc);
+    }
+}
+void Button_Save_EventHandler(Button* b,Component* c,ButtonEvent* e){
+    if(e->ButtonId == ALX_MOUSE_L && e->eid == EVENT_PRESSED){
+        printf("Save\n");
+    }
+}
+
+void Std_EventHandler(void* parent,void* r,EventId* e){
+    Scene* s = (Scene*)parent;
+    Renderable* ra = (Renderable*)r;
+    Component* c = Component_Scene_FindRR(&cg,ra);
+
+    if(c){
+        if(CStr_Cmp(c->name,"open")) Button_Open_EventHandler((Button*)ra,c,(ButtonEvent*)e);
+        if(CStr_Cmp(c->name,"save")) Button_Save_EventHandler((Button*)ra,c,(ButtonEvent*)e);
+
+        //Component_Print(&cg);
+        //Scene_Print(&scene);
+    }
+}
 
 void Setup(AlxWindow* w){
-    displays = Vector_New(sizeof(EDisplay));
+    scene = Scene_New();
 
-    Vector_Push(&displays,(EDisplay[]){ EDisplay_New(10.0f,10.0f,GetWidth() - 20.0f,GetHeight() - 20.0f,INPUT_MAXLENGTH,ALXFONT_PATHS_HIGH,32,64,SYNTAX_C,"./src/Main.c") });
+	cml = ComponentML_New(
+        KeywordMap_Make((KeywordRP[]){
+            KEYWORD_END
+        }),
+        OperatorMap_Make((OperatorRP[]){
+            OperatorRP_Make((TT_Type[]){ TOKEN_EQUAL_SIGN,TOKEN_END },                          TOKEN_CUSTOM_ASS,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_PLUS_SIGN,TOKEN_END },                           TOKEN_CUSTOM_ADD,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_MINUS_SIGN,TOKEN_END },                          TOKEN_CUSTOM_SUB,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_ASTERISK,TOKEN_END },                            TOKEN_CUSTOM_MUL,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_SLASH,TOKEN_END },                               TOKEN_CUSTOM_DIV,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_MINUS_SIGN,TOKEN_MINUS_SIGN,TOKEN_END },         TOKEN_CUSTOM_DEC,ARGS_IGNORE),
+            
+            OperatorRP_Make((TT_Type[]){ TOKEN_ANGLE_BRACKETS_L,TOKEN_END },                    TOKEN_CUSTOM_TAGL,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_ANGLE_BRACKETS_R,TOKEN_END },                    TOKEN_CUSTOM_TAGR,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_ANGLE_BRACKETS_L,TOKEN_SLASH,TOKEN_END },        TOKEN_CUSTOM_TAGC,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_ANGLE_BRACKETS_L,TOKEN_EXCLAMATION_MARK,
+                                         TOKEN_END },                                           TOKEN_CUSTOM_TAGLEX0,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_ANGLE_BRACKETS_L,TOKEN_EXCLAMATION_MARK,
+                                         TOKEN_MINUS_SIGN,TOKEN_END },                          TOKEN_CUSTOM_TAGLEX1,ARGS_IGNORE),
+    
+            OperatorRP_Make((TT_Type[]){ TOKEN_MINUS_SIGN,TOKEN_MINUS_SIGN,TOKEN_END },         TOKEN_CUSTOM_LINECOMMENT,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_ANGLE_BRACKETS_L,TOKEN_EXCLAMATION_MARK,
+                                         TOKEN_MINUS_SIGN,TOKEN_MINUS_SIGN,TOKEN_END },         TOKEN_CUSTOM_BLOCKCOMMENTSTART,ARGS_IGNORE),
+            OperatorRP_Make((TT_Type[]){ TOKEN_MINUS_SIGN,TOKEN_MINUS_SIGN,
+                                         TOKEN_ANGLE_BRACKETS_R,TOKEN_END },                    TOKEN_CUSTOM_BLOCKCOMMENTEND,ARGS_IGNORE),
+            OperatorRP_End()
+        }),
+        DirectiveMap_Make((Directive[]){
+            Directive_Comment_Block(TOKEN_CUSTOM_BLOCKCOMMENTSTART,TOKEN_CUSTOM_BLOCKCOMMENTEND,True),
+            Directive_Compress_Tag(TOKEN_CUSTOM_TAGOPEN,TOKEN_CUSTOM_TAGL,TOKEN_CUSTOM_TAGR),
+            Directive_Compress_Tag(TOKEN_CUSTOM_TAGCLOSE,TOKEN_CUSTOM_TAGC,TOKEN_CUSTOM_TAGR),
+            Directive_End()
+        }),
+        TT_TypeMap_Make((TT_Type[]){
+            TOKEN_NEWLINE,
+            TOKEN_END
+        }),
+        ReseterMap_Make((Reseter[]){ 
+            // operators who might have two versions: 1 arg vs. 2 args are not listed: -
+            { TOKEN_NONE,RESETER_NONE },
+            { TOKEN_NUMBER,RESETER_ADD1 },
+            { TOKEN_FLOAT,RESETER_ADD1 },
+            { TOKEN_STRING,RESETER_ADD1 },
+            
+            { TOKEN_SPACE,RESETER_NONE },
+            { TOKEN_NEWLINE,RESETER_NONE },// without semicolon like python
+            { TOKEN_CARTURN,RESETER_NONE },
+            { TOKEN_HTAB,RESETER_NONE },
+            { TOKEN_VTAB,RESETER_NONE },
+    
+            { TOKEN_COMMA,RESETER_RST },
+
+            { TOKEN_PARENTHESES_R,RESETER_TO1 },
+            { TOKEN_SQUARE_BRACKETS_R,RESETER_NONE },
+            { TOKEN_CURLY_BRACES_R,RESETER_NONE },
+            //{ TOKEN_ANGLE_BRACKETS_R,RESETER_NONE }, //also < and > operator
+            { TOKEN_DOUBLE_ANGLE_BRACKETS_R,RESETER_NONE },
+            { TOKEN_WHITE_SQUARE_BRACKETS_R,RESETER_NONE },
+            
+            { TOKEN_PARENTHESES_L,RESETER_RST },
+            { TOKEN_SQUARE_BRACKETS_L,RESETER_RST },
+            { TOKEN_CURLY_BRACES_L,RESETER_RST },
+            //{ TOKEN_ANGLE_BRACKETS_L,RESETER_RST }, //also < and > operator
+            { TOKEN_DOUBLE_ANGLE_BRACKETS_L,RESETER_RST },
+            { TOKEN_WHITE_SQUARE_BRACKETS_L,RESETER_RST },
+            { TOKEN_SEMICOLON,RESETER_RST },
+            { TOKEN_COLON,RESETER_RST },
+            { TOKEN_EXCLAMATION_MARK,RESETER_RST },
+            { TOKEN_QUESTION_MARK,RESETER_RST },
+            { TOKEN_ELLIPSIS,RESETER_RST },
+            { TOKEN_PLUS_SIGN,RESETER_RST },
+            { TOKEN_MULTIPLICATION_SIGN,RESETER_RST },
+            { TOKEN_DIVISION_SIGN,RESETER_RST },
+            { TOKEN_EQUAL_SIGN,RESETER_RST },
+            { TOKEN_NOT_EQUAL_SIGN,RESETER_RST },
+            { TOKEN_LESS_THAN,RESETER_RST },
+            { TOKEN_GREATER_THAN,RESETER_RST },
+            { TOKEN_LESS_THAN_OR_EQUAL_TO,RESETER_RST },
+            { TOKEN_GREATER_THAN_OR_EQUAL_TO,RESETER_RST },
+            { TOKEN_APPROXIMATION_SIGN,RESETER_RST },
+            { TOKEN_PLUS_MINUS_SIGN,RESETER_RST },
+            { TOKEN_AMPERSAND,RESETER_RST },
+            { TOKEN_AT_SIGN,RESETER_RST },
+            { TOKEN_ASTERISK,RESETER_RST },
+            { TOKEN_CARET,RESETER_RST },
+            { TOKEN_TILDE,RESETER_RST },
+            { TOKEN_SLASH,RESETER_RST },
+            { TOKEN_BACKSLASH,RESETER_RST },
+            { TOKEN_UNDERSCORE,RESETER_RST },
+            { TOKEN_PIPE,RESETER_RST },
+            { TOKEN_PERCENT,RESETER_RST },
+            { TOKEN_HASH_POUND_SIGN,RESETER_RST },
+            { TOKEN_SINGLE_QUOTE,RESETER_RST },
+            { TOKEN_DOUBLE_QUOTE,RESETER_RST },
+            { TOKEN_BACKTICK,RESETER_RST },
+            { TOKEN_PRIME,RESETER_RST },
+            { TOKEN_DOUBLE_PRIME,RESETER_RST },
+            { TOKEN_SECTION_SYMBOL,RESETER_RST },
+            { TOKEN_DEGREE_SYMBOL,RESETER_RST },
+            { TOKEN_FRACTION_SLASH,RESETER_RST },
+            { TOKEN_INTERROBANG,RESETER_RST },
+            { TOKEN_BULLET_POINT,RESETER_RST },
+    
+            { TOKEN_CAST,RESETER_ADD1 },
+            { TOKEN_FUNCTION,RESETER_ADD1 },
+            { TOKEN_CONSTSTRING_SINGLE,RESETER_ADD1 },
+            { TOKEN_CONSTSTRING_DOUBLE,RESETER_ADD1 },
+
+            { TOKEN_END,RESETER_RST }
+        })
+    );
+    
+    ComponentML_Source(&cml,"./code/Main.alxml");
+    //TokenMap_Print(&cml.tm);
+
+    cg = ComponentML_Build(&cml);
+    Component_Scene_Push_R(&cg,&scene);
+    Component_Scene_Event_R(&cg,&scene,Std_EventHandler);
+
+    fileinBuffer = NULL;
+    filesOpen = CVector_New(sizeof(CStr),(void*)CStr_Free,NULL,(void*)CStr_PCmp,(void*)CStr_print);
 }
 void Update(AlxWindow* w){
-    for(int i = 0;i<displays.size;i++){
-        EDisplay* d = (EDisplay*)Vector_Get(&displays,i);
-        EDisplay_Update(d,window.Strokes,GetMouse());
-    }
+    Scene_Update(&scene,window.Strokes,GetMouse(),GetMouseBefore());
 
-    Clear(BLACK);
+	Clear(BLACK);
 
-    for(int i = 0;i<displays.size;i++){
-        EDisplay* d = (EDisplay*)Vector_Get(&displays,i);
-        EDisplay_Render(WINDOW_STD_ARGS,d);
-    }
+	Scene_Render(WINDOW_STD_ARGS,&scene);
 }
 void Delete(AlxWindow* w){
-    Vector_Free(&displays);
+	Scene_Free(&scene);
+    Component_Free(&cg);
+    ComponentML_Free(&cml);
+
+    CStr_Free(&fileinBuffer);
+    CVector_Free(&filesOpen);
 }
 
+
+
 int main(int argc,char** argv){
-    if(Create("IDE - New",2000,1200,1,1,Setup,Update,Delete))
+    if(Create("IDE - New",1900,1000,1,1,Setup,Update,Delete))
         Start();
     return 0;
 }
